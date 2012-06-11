78ce59988d4d934d19ac098affb1f80da920a4dc
Vincent Debergue <vincent.debergue@gmail.com>
2012-06-11 17:55:57 +0200

    Vue formulaire intervenant + controleur

--- app/controllers/Intervenants.java
+++ app/controllers/Intervenants.java
@@ -1,9 +1,12 @@
 package controllers;
 
 import models.Intervenant;
+
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import views.html.*;
 
 public class Intervenants extends Controller {
 
@@ -18,15 +21,39 @@ public class Intervenants extends Controller {
 	}
 
 	public static Result edit(Long id) {
-		return TODO;
+		Intervenant in = Intervenant.find.byId(id);
+		if(in == null) {
+			//Si l'étudiant n'existe pas, on affiche une erreur
+			return badRequest("Erreur : Cet intervenant n'existe pas");
+		}
+		//On affiche un formulaire pré-rempli
+		return ok(IntervenantForm.render(blankForm.fill(in),true));
 	}
 
 	public static Result create() {
-		return TODO;
+		return ok(IntervenantForm.render(blankForm,false));
 	}
 
 	public static Result submit() {
-		return TODO;
+		//On récupère le formulaire depuis la requête
+		Form<Intervenant> filledForm = blankForm.bindFromRequest();
+		
+		//Si le formulaire a des erreurs, on remet le formulaire avec les erreurs.
+		if (filledForm.hasErrors()) {
+			return badRequest(IntervenantForm.render(filledForm,false));
+		}
+
+		Intervenant intervenant = filledForm.get();
+		Long id = intervenant.id;
+		//Si l'intervenant existe déjà on l'update
+		if(id != null){
+			intervenant.update();
+		}
+		else{
+			intervenant.save();
+		}
+		//On renvoie vers la liste des intervenants
+		return liste();
 	}
 
 	public static Result delete(Long id) {
