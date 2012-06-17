e72779532802c9ebfa5a212e2ad29eb994f044c5
Vincent Debergue <vincent.debergue@gmail.com>
2012-06-17 15:36:40 +0200

    Ajout de la dépendance docx4j

--- project/Build.scala
+++ project/Build.scala
@@ -9,10 +9,12 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      "org.docx4j" % "docx4j" % "2.8.0"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here
+        resolvers += "webjars" at "http://webjars.github.com/m2"
     )
 
 }
